6c1093bac7a0df1e71f53e44f2c7912649881a9e
misiek10000 <mkilian@mielec.com.pl>
2018-01-06 16:17:28 +0100

    Exercises 11.*
    11.1 - reading arguments from command line and generating n numbers to file
    11.2 - reading numbers from file and sorting it, then saving to file or prinitng screen

---
+++ JiMP-11.2-SortingNumbersFromFile/11.2-SortingNumbersFromFile.c
@@ -0,0 +1,81 @@
+//Zastosowano flagi:  -Wall -pedantic -Wextra -Werror -std=c99
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int openFile(char*,int*, int,char* argv[]);
+void saveNumbersToFile(char*,int*,int);
+void printSortedNumbers(int*,int);
+void errorsCheck(int);
+int compare(const void *, const void* );
+
+int main(int argc, char* argv[]){
+    errorsCheck(argc);
+
+    int* numbers = NULL;
+    openFile(argv[1],numbers,argc,argv);
+
+    return 0;
+}
+
+int openFile(char* filename,int* numbers,int argc,char* argv[]){
+    FILE *f = fopen(filename,"r");
+    int number = 0;
+    int counter = 0;
+    if(f == NULL){
+        printf("Blad podczas otwierania pliku!");
+        exit(1);
+    }
+    else{
+        while(fscanf(f,"%d",&number)!=EOF){
+            numbers = (int*)realloc(numbers,sizeof(int)*(counter+1));
+            *(numbers+counter)=number;
+            counter++;
+        }
+    }
+    fclose(f);
+
+    qsort(numbers,counter,sizeof(int),compare);
+
+    if(argc == 2){
+        printSortedNumbers(numbers,counter);
+    }
+    else{
+        saveNumbersToFile(argv[2],numbers,counter);
+    }
+
+    return counter;
+}
+
+void saveNumbersToFile(char* filename,int* sortedNumbers,int numberOfNumbers){
+    FILE *f = fopen(filename,"w");
+    if(f == NULL){
+        printf("Blad podczas otwierania pliku!");
+    }
+    else{
+        for (int i = 0; i < numberOfNumbers; ++i){
+            fprintf(f,"%d\n",sortedNumbers[i]);
+        }
+    }
+    fclose(f);
+
+}
+
+void printSortedNumbers(int* sortedNubmers,int numberOfNumbers){
+    for(int i = 0; i < numberOfNumbers; ++i){
+        printf("%d\n",*(sortedNubmers+i));
+    }
+}
+
+void errorsCheck(int argc){
+    if(argc < 2 || argc > 3){
+        printf("Podano bledno ilosc argumentow!");
+        exit(1);
+    }
+}
+
+int compare(const void * a, const void * b){
+    if ( *(int*)a <  *(int*)b ) return -1;
+    else if ( *(int*)a == *(int*)b ) return 0;
+    else return 1;
+}
---
+++ JiMP_11.1-SavingRandomNumbers/11.1_SavingRandomNumbers.c
@@ -0,0 +1,54 @@
+//Zastosowano flagi:  -Wall -pedantic -Wextra -Werror -std=c99
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+void generateRandomNumbers(int,int[]);
+void saveNumbersToFile(char*,int*,int);
+void errorsCheck(int,int);
+
+
+int main(int argc, char*argv[]){
+
+    srand(time(NULL));
+    int numberOfNumbers = atoi(argv[1]);
+    errorsCheck(argc,numberOfNumbers);
+    int randomNumbers[numberOfNumbers];
+
+    generateRandomNumbers(numberOfNumbers,randomNumbers);
+    saveNumbersToFile(argv[2],randomNumbers,numberOfNumbers);
+
+    return 0;
+}
+
+void generateRandomNumbers(int numberOfNumbers,int randomNumbers[]){
+    for(int i = 0; i < numberOfNumbers; ++i){
+        randomNumbers[i]=rand()%1001;
+    }
+}
+
+void saveNumbersToFile(char* filename,int* randomNumbers,int numberOfNumbers){
+    FILE *f = fopen(filename,"w");
+    if(f == NULL){
+        printf("Blad podczas otwierania pliku!");
+    }
+    else{
+        for (int i = 0; i < numberOfNumbers; ++i){
+            fprintf(f,"%d\n",randomNumbers[i]);
+        }
+    }
+    fclose(f);
+}
+
+void errorsCheck(int argc,int numberOfNumbers){
+    if(argc != 3){
+        printf("Podano bledno ilosc argumentow!");
+        exit(1);
+    }
+    else if(numberOfNumbers <=0){
+        printf("Blednie podano ilosc liczb do wygenerowania!");
+        exit(2);
+    }
+}
